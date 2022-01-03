23e90373aa5dfa944eb94f27a4b3bf0d48882830
Jonas Berlin <xkr47@outerspace.dyndns.org>
2022-01-04 00:50:54 +0200

    Reorder & refactor

--- src/main.rs
+++ src/main.rs
@@ -13,36 +13,66 @@ use xcb::x::GrabMode;
 use xcb::x::ModMask;
 use xcb::x::Screen;
 
+const DEVICE: &str = "default";
 const CONTROL: &str = "Capture";
 const UNMUTE_DELAY_MS: u64 = 150;
 
 fn main() {
-    let x_conn = open_x_and_listen_to_hotkey().expect("Failed to setup X11");
-
     let expected_capture_state = Arc::new(AtomicBool::new(false));
+
     {
         let expected_capture_state = expected_capture_state.clone();
         thread::spawn(move || {
-            let alsa_mixer = Mixer::new("default", false).expect("Failed to setup alsa");
-            let selem = get_alsa_mixer_capture_elem(&alsa_mixer).expect("Failed to find recording channel");
-            loop {
-                let capture_enabled = 0 != selem.get_capture_switch(SelemChannelId::FrontLeft).expect("Could not get capture switch value");
-                println!("capture_enabled = {}", capture_enabled);
-                if capture_enabled != expected_capture_state.load(Ordering::Acquire) {
-                    let expected = !capture_enabled;
-                    println!("fixing to {}", expected);
-                    if let Err(e) = set_capture_state(&selem, expected) {
-                        print!("Error fixing: {:?}", e);
-                    }
-                }
-                alsa_mixer.wait(None).expect("alsa_mixer.wait() failed");
-                alsa_mixer.handle_events().expect("alsa_mixer.handle_events() failed");
-            }
+            enforce_mixer_capture_state(expected_capture_state)
         });
     }
 
-    let alsa_mixer = Mixer::new("default", false).expect("Failed to setup alsa");
-    let selem = get_alsa_mixer_capture_elem(&alsa_mixer).expect("Failed to find recording channel");
+    listen_to_keyboard_events_and_update_mixer(expected_capture_state)
+}
+
+// -------------
+
+fn enforce_mixer_capture_state(expected_capture_state: Arc<AtomicBool>) -> ! {
+    let alsa_mixer = Mixer::new(DEVICE, false).expect("Failed to setup alsa");
+    let mixer_capture_elem = get_alsa_mixer_capture_elem(&alsa_mixer).expect("Failed to find recording channel");
+    loop {
+        let capture_enabled = 0 != mixer_capture_elem.get_capture_switch(SelemChannelId::FrontLeft).expect("Could not get capture switch value");
+        let expected = expected_capture_state.load(Ordering::Acquire);
+        if capture_enabled != expected {
+            println!("Fixing capture state to {}", if expected { "unmuted" } else { "muted" });
+            if let Err(e) = set_capture_state(&mixer_capture_elem, expected) {
+                println!("- Error fixing: {:?}", e);
+            }
+        }
+        alsa_mixer.wait(None).expect("alsa_mixer.wait() failed");
+        alsa_mixer.handle_events().expect("alsa_mixer.handle_events() failed");
+    }
+}
+
+fn get_alsa_mixer_capture_elem<'a>(alsa_mixer: &'a Mixer) -> Result<Selem<'a>, Box<dyn Error>> {
+    let mixer_capture_elem = alsa_mixer.find_selem(&SelemId::new(CONTROL, 0)).ok_or_else(|| GenericError(format!("Could not find simple control {}", CONTROL)))?;
+    if !mixer_capture_elem.has_capture_switch() {
+        Err(GenericError("Capture switch not found, cannot adjust"))?;
+    }
+    Ok(mixer_capture_elem)
+}
+
+fn set_capture_state(mixer_capture_elem: &Selem<'_>, state: bool) -> Result<(), Box<dyn Error>> {
+    let state = if state { 1 } else { 0 };
+    for channel in SelemChannelId::all() {
+        mixer_capture_elem.set_capture_switch(*channel, state)?;
+    }
+    Ok(())
+}
+
+// -------------
+
+fn listen_to_keyboard_events_and_update_mixer(expected_capture_state: Arc<AtomicBool>) {
+    let alsa_mixer = Mixer::new(DEVICE, false).expect("Failed to setup alsa");
+    let mixer_capture_elem = get_alsa_mixer_capture_elem(&alsa_mixer).expect("Failed to find recording channel");
+
+    let x_conn = open_x_and_listen_to_hotkey().expect("Failed to setup X11");
+
     loop {
         let event = x_conn.wait_for_event();
         #[allow(unreachable_patterns)]
@@ -61,49 +91,18 @@ fn main() {
             Event::KeyPress(_) => {
                 println!("Unmuting");
                 thread::sleep(Duration::from_millis(UNMUTE_DELAY_MS));
-                expected_capture_state.store(true, Ordering::Release);
-                if let Err(e) = set_capture_state(&selem, true) {
-                    print!("Error setting mixer capture state: {:?}", e);
-                }
+                set_expected_capture_state(&expected_capture_state, &mixer_capture_elem, true);
             }
             Event::KeyRelease(_) => {
                 println!("Muting");
-                expected_capture_state.store(false, Ordering::Release);
-                if let Err(e) = set_capture_state(&selem, false) {
-                    print!("Error setting mixer capture state: {:?}", e);
-                }
+                set_expected_capture_state(&expected_capture_state, &mixer_capture_elem, false);
             }
             _ => {
                 println!("Unhandled event, ignoring — {:#?}", event);
             }
         }
-    }
-}
-
-fn set_capture_state(selem: &Selem<'_>, state: bool) -> Result<(), Box<dyn Error>> {
-    let state = if state { 1 } else { 0 };
-    for channel in SelemChannelId::all() {
-        selem.set_capture_switch(*channel, state)?;
-    }
-    Ok(())
-}
-
-fn get_alsa_mixer_capture_elem<'a>(alsa_mixer: &'a Mixer) -> Result<Selem<'a>, Box<dyn Error>> {
-    let selem = alsa_mixer.find_selem(&SelemId::new(CONTROL, 0)).ok_or_else(|| GenericError(format!("Could not find simple control {}", CONTROL)))?;
-    if !selem.has_capture_switch() {
-        Err(GenericError("Capture switch not found, cannot adjust"))?;
-    }
-    Ok(selem)
-}
-
-#[derive(Debug)]
-struct GenericError<S: AsRef<str> + Display + Debug>(S);
-
-impl<S: AsRef<str> + Display + Debug> Error for GenericError<S> {}
-
-impl<S: AsRef<str> + Display + Debug> Display for GenericError<S> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "GenericError: {}", self.0)
+        // just in case they otherwise pile up somewhere
+        alsa_mixer.handle_events().expect("alsa_mixer.handle_events() failed");
     }
 }
 
@@ -122,4 +121,24 @@ fn open_x_and_listen_to_hotkey() -> Result<Connection, Box<dyn Error>> {
     });
     conn.flush()?;
     Ok(conn)
+}
+
+fn set_expected_capture_state(expected_capture_state: &Arc<AtomicBool>, mixer_capture_elem: &Selem, state: bool) {
+    expected_capture_state.store(state, Ordering::Release);
+    if let Err(e) = set_capture_state(&mixer_capture_elem, state) {
+        print!("Error setting mixer capture state: {:?}", e);
+    }
+}
+
+// -------------
+
+#[derive(Debug)]
+struct GenericError<S: AsRef<str> + Display + Debug>(S);
+
+impl<S: AsRef<str> + Display + Debug> Error for GenericError<S> {}
+
+impl<S: AsRef<str> + Display + Debug> Display for GenericError<S> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "GenericError: {}", self.0)
+    }
 }
